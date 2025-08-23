249544172216d4646d28afa2abdcb2c7d1e5c506
Mateusz Toczyski <mtoczyski303@gmail.com>
2025-08-23 10:51:25 +0200

    working on audio instalation

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
+import sys
 import os
-import pprint
+import pathlib
 import googleapiclient.discovery
-def pull_metadata():
+import yt_dlp
+
+BASE_PATH = pathlib.Path('.')
+
+def pull_metadata() -> dict:
     try:
         youtube = googleapiclient.discovery.build(serviceName="youtube", version="v3", developerKey=os.environ.get("google_cloud_api_key"))
         req = youtube.search().list(
             part="snippet",
-            maxResults=10,
+            maxResults=1,
             videoLicense="creativeCommon",
             q="english lessons",
             type="video",
@@ -16,10 +21,10 @@ def pull_metadata():
         return req.execute()
     except Exception as e:
         print(f"Something went wrong while pulling metadata, ErrorMessage: {e}")
-        return
+        return {}
 
-def get_urls(d: dict):
-    videos = d.get('items', None)
+def get_urls(data: dict) -> list:
+    videos = data.get('items', None)
     if videos != None:
         urls = []
         for video in videos:
@@ -29,11 +34,24 @@ def get_urls(d: dict):
         print("Got urls")
         return urls
     print("Something went wrong in get_urls()")
-    return None
+    return []
+
+def download_audios(urls: list) -> None:
+    AUDIO_PATH = BASE_PATH / 'audios'
+    if AUDIO_PATH.exists() == False:
+        q = str(input("Do you want to create folder for audios(Y/n)"))
+        if q == "Y":
+            AUDIO_PATH.mkdir()
+        elif q == "n":
+            print("Canceling installation")
+            sys.exit(1)
+    return
+
 
 if __name__ == "__main__":
-    data = pull_metadata()
-    if data != None:
-        urls = get_urls(data)
-    else:
-        urls = None
+    try:
+        data = pull_metadata()
+        urls = get_urls(data=data)
+        download_audios(urls=urls)
+    except Exception as e:
+        print(f"Something went wrong.\n{e}")
