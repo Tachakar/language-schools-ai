3f4ceadb07ca03ae063995fe72c3ed4564c79632
Tachakar <mtoczyski303@gmail.com>
2025-08-26 16:44:19 +0200

    started work on data preprocessing

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ from faster_whisper import WhisperModel
 BASE_PATH = pathlib.Path('.')
 AUDIO_PATH = BASE_PATH / 'audios'
 TRANSCRIPT_PATH = BASE_PATH / 'transcripts'
+TRANSCRIPT_RAW_PATH = TRANSCRIPT_PATH / 'raw'
+TRANSCRIPT_CLEAN_PATH = TRANSCRIPT_PATH / 'clean'
 
 YDL_OPTS = {
             "format": "bestaudio",
@@ -22,14 +24,14 @@ YDL_OPTS = {
 
 MAX_RESULTS = 3
 VIDEO_LICENSE = "creativeCommon"
-Q = "english lessons"
+QUERY = "english lessons"
 
 
 def check_api_key() -> None:
     if os.environ.get("YOUTUBE_API_KEY", None) == None:
         sys.exit("Coulnd't get youtube api key")
 
-def check_path(path:pathlib.Path) -> None:
+def check_dir_path(path:pathlib.Path) -> None:
     path.mkdir(parents=True, exist_ok=True)
 
 def fetch_youtube_metadata() -> dict:
@@ -39,7 +41,7 @@ def fetch_youtube_metadata() -> dict:
             part="snippet",
             maxResults=MAX_RESULTS,
             videoLicense=VIDEO_LICENSE,
-            q=Q,
+            q=QUERY,
             type="video",
             videoDuration="medium",
         )
@@ -64,19 +66,24 @@ def get_urls(data: dict) -> list:
 def download_wav_files(urls: list) -> None:
     if urls == []:
         sys.exit("URLs list is empty")
-    check_path(path=AUDIO_PATH)
+    check_dir_path(path=AUDIO_PATH)
     with yt_dlp.YoutubeDL(YDL_OPTS) as ydl:
         ydl.download(urls)
 
 def make_transcripts():
-    check_path(path=TRANSCRIPT_PATH)
-    model = WhisperModel("large-v2", device='cuda')
-    results = []
+    check_dir_path(path=TRANSCRIPT_PATH)
+    check_dir_path(path=TRANSCRIPT_RAW_PATH)
+    check_dir_path(path=TRANSCRIPT_CLEAN_PATH)
+    model = WhisperModel("large-v2", device='cuda', compute_type='float16')
     for wav_file in AUDIO_PATH.iterdir():
-        result, _ = model.transcribe(str(wav_file.absolute()))
-        for segment in result:
-            print("[%.2fs -> %.2fs] %s" % (segment.start, segment.end, segment.text))
-
+        vid_id = wav_file.stem
+        out_raw_path = TRANSCRIPT_RAW_PATH / f'{vid_id}_raw.txt'
+        if out_raw_path.exists() or wav_file.suffix != '.wav':
+            continue
+        with open(out_raw_path, mode='w', encoding='UTF-8') as raw_txt_file:
+            segments, _ = model.transcribe(str(wav_file.absolute()), language='en')
+            for segment in segments:
+                raw_txt_file.write("[%.2f:%.2f] %s \n" % (segment.start, segment.end, segment.text))
 
 if __name__ == "__main__":
     try:
